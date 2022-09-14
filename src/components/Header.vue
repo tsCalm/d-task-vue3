0027733b1a052305230bd9575ff8be6230ea67c0
tsCalm <dlatkdals93@gmail.com>
2022-09-14 19:27:14 +0900

    update: Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,24 @@
 <template>
   <header id="header">
-    <div class="header__title">BOARD</div>
-    <div class="header__option">ICON</div>
+    <div class="title">BOARD</div>
+    <div class="option">ICON</div>
   </header>
 </template>
 
 <script setup lang="ts"></script>
-<style scoped>
+<style lang="scss" scoped>
 #header {
   width: 100%;
   height: 100%;
   max-height: 80px;
+  padding: 16px 32px;
   border: 1px solid red;
   display: flex;
-  /* justify-content: space-around; */
+  align-items: center;
+  justify-content: space-between;
+  & .title {
+    font-size: 16;
+    font-weight: bold;
+  }
 }
 </style>
