67bb734ffffc09994ccc531b03c1057433121226
tsCalm <dlatkdals93@gmail.com>
2022-09-20 16:59:58 +0900

    feat: anchor tag

--- src/components/aside/MyPage.vue
+++ src/components/aside/MyPage.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>mypage</div>
+  <div class="my-page">
+    <div class="my-page__header">
+      <div class="title bold">마이페이지</div>
+      <Icon name="close" @click="SET_CURRENT_ASIDE('CHAT')" />
+    </div>
+  </div>
 </template>
 <script>
-export default {};
+import Icon from "@/components/basic/Icon.vue";
+import { mapGetters, mapMutations, mapActions } from "vuex";
+export default {
+  components: {
+    Icon,
+  },
+  methods: {
+    ...mapMutations("header", ["SET_CURRENT_ASIDE"]),
+  },
+};
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+@import "@/style";
+.my-page {
+  &__header {
+    padding: 24px 16px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid var(--color-gray-10);
+    & > .title {
+      font-size: 18px;
+      line-height: 21px;
+    }
+  }
+}
+</style>
---
+++ src/components/basic/Anchor.vue
@@ -0,0 +1,23 @@
+<template>
+  <a class="anchor" :href="href">
+    {{ text }}
+  </a>
+</template>
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.anchor {
+}
+</style>
